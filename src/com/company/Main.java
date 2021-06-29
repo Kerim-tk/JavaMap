23aa05c8a984dfd59f163454587da1579dbf3e0a
Kerim-tk <alekperovkerim@gmail.com>
2021-06-29 10:29:36 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+
+class Solution{
+    public static void main(String []argh)
+    {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        Map m = new HashMap();
+        in.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            in.nextLine();
+            m.put(name,phone);
+        }
+        while(in.hasNext())
+        {
+            String s=in.nextLine();
+            if (m.containsKey(s)) {
+                System.out.println(s + "=" + m.get(s));
+            }
+            else {
+                System.out.println("Not found");
+            }
+        }
+    }
+}
